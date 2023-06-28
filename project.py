b2c3659eaf9593855c6f682b6e7bc633b5269c1c
meetdonga2125 <133622404+meetdonga2125@users.noreply.github.com>
2023-06-28 09:00:51 +0530

    oops_project.py

--- project.py
+++ project.py
@@ -197,71 +197,11 @@
 #                 exit()
 #             elif user_choice2 == "c":
 #                 continue    
+                                                        
                 
             
         
 
-
-
-#-----------------------Game-----------------------##
-
-import random
-
-COLORS = ["R", "G", "B", "Y", "W", "O"]
-TRIES = 10
-CODE_LENGTH = 4
-
-def generate_code():
-    code = []
-    
-    for _ in range(CODE_LENGTH):
-        color = random.choice(COLORS)
-        code.append(color)
-
-    return code
-
-# print(generate_code())    
-
-def guess_code():
-    while True:
-        guess = input("Guess: ").upper().split(" ")
-    
-        if len(guess) != CODE_LENGTH:
-            
-           print(f'You must guess {CODE_LENGTH} colors.')
-           continue
-        
-        for color in guess:
-            if color not in COLORS:
-                print(f'Invalid color: {color}. Try again')
-                break
-        else:
-            break
-    
-    return guess
-
-print(guess_code())         
-
-
-def check_code(guess, real_code):
-    color_counts = {}
-    correct_pos = 0
-    incorrect_pos = 0  
-    
-    for color in real_code:
-        if color not in color_counts:
-            color_counts[color] = 0
-        color_counts[color] += 1
-        
-    for guess_color, real_color in zip(guess, real_code):
-        if guess_color == real_color:
-            correct_pos += 1
-            color_counts[guess_color] -= 1
-            
-                    
-for i in range(10):
-    print(i)
-    break
 
 
     
